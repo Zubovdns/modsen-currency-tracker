f714cb5192a83b9d68339a1edaf61be02069d1f7
Monika <zubovdns@icloud.com>
2024-06-26 20:35:53 +0300

    feat/added mobile version of title

--- src/components/Header/Navbar/Title/styled.ts
+++ src/components/Header/Navbar/Title/styled.ts
@@ -1,3 +1,4 @@
+import { device } from '@constants/breakpoints'
 import styled from 'styled-components'
 
 export const TitleContainer = styled.div`
@@ -14,6 +15,10 @@ export const TitleContainer = styled.div`
     #121212 11.77%,
     rgba(36, 121, 64, 0) 91.12%
   );
+
+  @media ${device.md} {
+    height: 40vw;
+  }
 `
 
 export const TitleTextContainer = styled.div`
@@ -21,10 +26,16 @@ export const TitleTextContainer = styled.div`
   flex-direction: column;
   align-items: flex-end;
   flex: 1;
+
+  @media ${device.md} {
+    align-items: center;
+    justify-content: center;
+  }
 `
 
 export const TitleMainText = styled.h1`
   font-size: 5vw;
+
   background: linear-gradient(
     90.18deg,
     #00ce2c 0.18%,
@@ -34,17 +45,33 @@ export const TitleMainText = styled.h1`
   -webkit-background-clip: text;
   background-clip: text;
   color: transparent;
+
+  @media ${device.md} {
+    font-size: 7vw;
+  }
 `
 
 export const TitleSubText = styled.p`
   width: 17vw;
-  font-size: 1.1vw;
+  font-size: 1.2vw;
   color: #d9d9d9;
   text-align: center;
+  margin-top: 2vw;
+
+  @media ${device.md} {
+    width: 30vw;
+    line-height: 4vw;
+    font-size: 2vw;
+    margin-top: 4vw;
+  }
 `
 
 export const TitleLogoContainer = styled.div`
   flex: 0.8;
+
+  @media ${device.md} {
+    display: none;
+  }
 `
 export const TitleLogo = styled.img`
   width: 20vw;
