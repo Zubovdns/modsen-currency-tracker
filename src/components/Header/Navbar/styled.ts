93b96acfc7eb962f4f875d5c210c4ac9a88f9b19
Monika <zubovdns@icloud.com>
2024-06-26 19:10:46 +0300

    feat/ added link hover animations

--- src/components/Header/Navbar/styled.ts
+++ src/components/Header/Navbar/styled.ts
@@ -1,3 +1,4 @@
+import { device } from '@constants/breakpoints'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
@@ -14,7 +15,19 @@ export const NavbarContainer = styled.nav`
 export const NavbarLink = styled(Link)`
   font-size: 1.5vw;
   color: white;
-  font-family: Arial, sans-serif;
+
+  @media ${device.md} {
+    font-size: 2.5vw;
+    line-height: 120%;
+  }
+
+  @media ${device.sm} {
+    font-size: 2.7vw;
+  }
+
+  @media ${device.xs} {
+    font-size: 3vw;
+  }
 `
 
 export const NavbarLinksList = styled.ul`
@@ -25,6 +38,11 @@ export const NavbarLinksList = styled.ul`
 
 export const NavbarLinksListItem = styled.li`
   margin: 0;
+  transition: transform 0.3s ease;
+
+  &:hover {
+    transform: scale(1.1);
+  }
 `
 
 export const NavbarLogo = styled.img`
