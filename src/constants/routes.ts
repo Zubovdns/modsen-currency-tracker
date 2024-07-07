3ab2ec1a89719f18e26ea1d550535b740f3f7c69
Monika <zubovdns@icloud.com>
2024-07-07 17:19:25 +0300

    feat/added contact route

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,6 +2,7 @@ import { lazy } from 'react'
 
 const Home = lazy(() => import('@pages/Home'))
 const Timeline = lazy(() => import('@pages/Timeline'))
+const Contact = lazy(() => import('@pages/Contact'))
 
 export const HOME_ROUTE = '/'
 export const TIMELINE_ROUTE = '/timeline'
@@ -12,5 +13,5 @@ export const routes = [
   { path: HOME_ROUTE, Page: Home },
   { path: TIMELINE_ROUTE, Page: Timeline },
   { path: BANK_CARD_ROUTE, Page: 'div' },
-  { path: CONTACT_ROUTE, Page: 'div' },
+  { path: CONTACT_ROUTE, Page: Contact },
 ]
---
+++ src/pages/Contact/index.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react'
+import { ErrorBoundary } from '@components/ErrorBoundary'
+
+const Contact: FC = () => <ErrorBoundary>div</ErrorBoundary>
+
+export default Contact
