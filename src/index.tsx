cbd6ee3f4850afa9ef075cfb52a72fb3d730edc1
Monika <zubovdns@icloud.com>
2024-07-15 16:39:24 +0300

    fix/wrapped app component with error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,7 @@ import { PersistGate } from 'redux-persist/integration/react'
 import { ThemeProvider } from 'styled-components'
 
 import 'mapbox-gl/dist/mapbox-gl.css'
+import { ErrorBoundary } from '@components/ErrorBoundary'
 
 const MainComponent = () => {
   const theme = useAppSelector(themeMode)
@@ -29,11 +30,13 @@ const MainComponent = () => {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <MainComponent />
-        </PersistGate>
-      </Provider>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <MainComponent />
+          </PersistGate>
+        </Provider>
+      </ErrorBoundary>
     </BrowserRouter>
   </React.StrictMode>
 )
